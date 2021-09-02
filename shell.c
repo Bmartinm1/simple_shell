11f54ec0f3fc836627295567a16593866ed7e9b8
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-09-01 21:41:16 -0400

    betty: shell.c

--- shell.c
+++ shell.c
@@ -4,35 +4,34 @@
  * main - simple shell
  * @ac: arg count
  * @av: arg variables
- * @env: env variables
- * 
+ * @environ: env variables
+ *
  * Return: Hopefully some completed commands
  */
 
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)), char **environ)
 {
-  int loop = 1;
-  char *input = NULL;
-  size_t length;
+	int loop = 1;
+	char *input = NULL;
+	size_t length;
 
-  if (isatty(STDIN_FILENO) == 1)
-    ;
+	if (isatty(STDIN_FILENO) == 1);
 
-  while (loop)
-  {
-    initPrompt();
+	while (loop)
+	{
+		initPrompt();
 
-    if (getline(&input, &length, stdin) == -1)
-      perror("We've got an error!\n");
+		if (getline(&input, &length, stdin) == -1)
+			perror("We've got an error!\n");
 
-    if (_strcmp(input, "exit\n") == 0)
-    {
-      loop = 0;
-      exit(0);
-    }
-
-    else if (_strcmp(input, "env\n") == 0)
-      fetchenv(environ);
-  }
-  return (0);
+		if (_strcmp(input, "exit\n") == 0)
+		{
+			loop = 0;
+			exit(0);
+		}
+		
+		else if (_strcmp(input, "env\n") == 0)
+ 			fetchenv(environ);
+	}
+	return (0);
 }
